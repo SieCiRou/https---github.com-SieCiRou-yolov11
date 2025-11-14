840b729f7663ba4e1c28502671ef8a5da89a76a9
CIROU_uuna <ae1413191@gmail.com>
2025-11-14 09:19:47 +0800

    測試torch中device設置

---
+++ Yolov11_1113/test_env.py
@@ -0,0 +1,8 @@
+import torch
+
+if torch.cuda.is_available():
+    print("CUDA 可用。")
+    print(f"可用的 GPU 數量：{torch.cuda.device_count()}")
+    print(f"當前 GPU 名稱：{torch.cuda.get_device_name(0)}") # 獲取第一個 GPU 的名稱
+else:
+    print("CUDA 不可用，請使用 device='cpu'。")
